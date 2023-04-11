6bcbcd8b811874dd5b4310d91ea4a9234df91000
Sangam Birajdar <Birajdars24@gmail.com>
2023-04-11 11:35:19 +0530

    initial commit

---
+++ pipeline-config.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent any
+
+    environment {
+        CURRENT_VERSION = currentVersion()
+        NEXT_VERSION = nextVersion()
+    }
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo "current vesion = ${CURRENT_VERSION}"
+                echo "next version = ${NEXT_VERSION}"
+            }
+        }
+    }
+}
